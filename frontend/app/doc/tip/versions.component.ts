56a187a8dd39b5e9aeb325bb87d694cb1d7a6522
Gyu-Ho Lee <gyuhox@gmail.com>
2016-09-29 13:16:44 -0700

    frontend: add routable doc link

--- frontend/app/doc/tip/versions.component.ts
+++ frontend/app/doc/tip/versions.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { parentComponent } from './common.component';
 
 export class versionItem {
+    isRoutable: boolean;
     version: string;
     versionHTMLClassLinkRelease: string;
     releaseLink: string;
     docLink: string;
     versionHTMLClassLinkDoc: string;
-    constructor(version: string, releaseLink: string, versionHTMLClassLinkRelease: string, docLink: string, versionHTMLClassLinkDoc: string) {
+    constructor(isRoutable: boolean, version: string, releaseLink: string, versionHTMLClassLinkRelease: string, docLink: string, versionHTMLClassLinkDoc: string) {
+        this.isRoutable = isRoutable;
         this.version = version;
         this.releaseLink = releaseLink;
         this.versionHTMLClassLinkRelease = versionHTMLClassLinkRelease;
@@ -38,13 +40,15 @@ export class versions_tip_Component extends parentComponent {
         this.majorVersionItems = [
             new majorVersionItem('v3', [
                 new versionItem(
-                    'tip (master branch)',
+                    true,
+                    'tip',
                     'https://github.com/coreos/etcd',
                     'versions-link-main',
                     '/doc/tip',
                     'versions-link-other'
                 ),
                 new versionItem(
+                    false,
                     'v3.1.0',
                     'https://github.com/coreos/etcd/releases/tag/v3.1.0',
                     'versions-link-main-emphasize',
@@ -52,6 +56,7 @@ export class versions_tip_Component extends parentComponent {
                     'versions-link-other'
                 ),
                 new versionItem(
+                    false,
                     'v3.0.10',
                     'https://github.com/coreos/etcd/releases/tag/v3.0.10',
                     'versions-link-main',
@@ -59,6 +64,7 @@ export class versions_tip_Component extends parentComponent {
                     'versions-link-other'
                 ),
                 new versionItem(
+                    false,
                     'v3.0.0',
                     'https://github.com/coreos/etcd/releases/tag/v3.0.0',
                     'versions-link-main',
@@ -68,6 +74,7 @@ export class versions_tip_Component extends parentComponent {
             ]),
             new majorVersionItem('v2', [
                 new versionItem(
+                    false,
                     'v2.3.7',
                     'https://github.com/coreos/etcd/releases/tag/v2.3.7',
                     'versions-link-main',
