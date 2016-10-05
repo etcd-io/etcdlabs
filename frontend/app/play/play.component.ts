b2946c45c009d3f3cc56acdb258f9c8147545c93
Gyu-Ho Lee <gyuhox@gmail.com>
2016-10-04 17:02:06 -0700

    frontend: add 'NodeStatus' class to play

--- frontend/app/play/play.component.ts
+++ frontend/app/play/play.component.ts
@@ -1,5 +1,40 @@
 import { Component } from '@angular/core';
 
+
+export class NodeStatus {
+  Name: string;
+  ID: string;
+  Endpoint: string;
+
+  IsLeader: boolean;
+  State: string;
+
+  DBSize: number;
+  DBSizeTxt: string;
+  Hash: number;
+
+  constructor(
+    name: string,
+    id: string,
+    endpoint: string,
+    isLeader: boolean,
+    state: string,
+    dbSize: number,
+    dbSizeTxt: string,
+    hash: number) {
+      this.Name = name;
+      this.ID = id;
+      this.Endpoint = endpoint;
+
+      this.IsLeader = isLeader;
+      this.State = state;
+
+      this.DBSize = dbSize;
+      this.DBSizeTxt = dbSizeTxt;
+      this.Hash = hash;
+  }
+}
+
 @Component({
   selector: 'play',
   templateUrl: 'play.component.html',
@@ -7,9 +42,18 @@ import { Component } from '@angular/core';
 })
 export class PlayComponent {
   selectedTab: number;
+  nodeStatuses: NodeStatus[];
   constructor() {
-    this.selectedTab = 2;
+    this.selectedTab = 3;
+    this.nodeStatuses = [
+      new NodeStatus('node1', 'None', 'None', false, 'Follower', 0, '0 B', 0),
+      new NodeStatus('node2', 'None', 'None', false, 'Follower', 0, '0 B', 0),
+      new NodeStatus('node3', 'None', 'None', false, 'Follower', 0, '0 B', 0),
+      new NodeStatus('node4', 'None', 'None', false, 'Follower', 0, '0 B', 0),
+      new NodeStatus('node5', 'None', 'None', false, 'Follower', 0, '0 B', 0),
+    ];
   }
+
   selectTab(num: number) {
     this.selectedTab = num;
   }
