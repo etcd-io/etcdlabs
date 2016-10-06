bdb00045f872c0b974277d405f5c85beae314a7b
Gyu-Ho Lee <gyuhox@gmail.com>
2016-10-06 15:22:25 -0700

    script: update start

--- script-start-etcdlabs.sh
+++ script-start-etcdlabs.sh
@@ -8,10 +8,11 @@ echo "running backend etcdlabs"
 nohup etcdlabs >> $HOME/etcdlabs.log 2>&1 &
 
 sleep 3s
-
 echo "starting frontend"
 nohup npm start >> $HOME/etcdlabs-npm.log 2>&1 &
 
+sleep 2s
+cat $HOME/etcdlabs-npm.log
 cat $HOME/etcdlabs.log
-tail -f $HOME/etcdlabs.log
 # tail -f $HOME/etcdlabs-npm.log
+# tail -f $HOME/etcdlabs.log
