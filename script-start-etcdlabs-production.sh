cd4f7f2d74f1f6df2e4aa4564ae6f4a499dc85e9
Gyu-Ho Lee <gyuhox@gmail.com>
2016-10-09 00:48:37 -0700

    script: add production deploy script

---
+++ script-start-etcdlabs-production.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -ex
+
+echo "restarting nginx"
+sudo apt-get install -y nginx
+sudo service nginx stop
+sudo cp ./nginx.conf /etc/nginx/sites-available/default
+sudo service nginx restart
+
+echo "building backend ectcdlabs"
+go build -v
+
+echo "running backend etcdlabs"
+nohup ./etcdlabs >> $HOME/etcdlabs.log 2>&1 &
+
+sleep 3s
+echo "starting frontend"
+nohup npm start >> $HOME/etcdlabs-npm.log 2>&1 &
+
+sleep 2s
+cat $HOME/etcdlabs-npm.log
+cat $HOME/etcdlabs.log
+# tail -f $HOME/etcdlabs-npm.log
+# tail -f $HOME/etcdlabs.log
