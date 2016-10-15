5ad06f74027cff3cdcd8ba57ac715ce764342e64
Gyu-Ho Lee <gyuhox@gmail.com>
2016-10-15 10:57:33 -0700

    script: update local start script

--- script-etcdlabs-start-local.sh
+++ script-etcdlabs-start-local.sh
@@ -7,16 +7,15 @@ go build -v
 echo "running backend etcdlabs"
 nohup ./etcdlabs >> /tmp/etcdlabs.log 2>&1 &
 
-sleep 3s
+sleep 5s
 echo "starting frontend"
 nohup npm start >> /tmp/etcdlabs-npm.log 2>&1 &
 
-sleep 2s
-cat /tmp/etcdlabs-npm.log
+sleep 5s
 cat /tmp/etcdlabs.log
+cat /tmp/etcdlabs-npm.log
 
 <<COMMENT
-tail -f /tmp/etcdlabs-npm.log
 tail -f /tmp/etcdlabs.log
+tail -f /tmp/etcdlabs-npm.log
 COMMENT
-
