dea052705c9dbce3d9fd6ac9616045a9cb96bce0
Gyu-Ho Lee <gyuhox@gmail.com>
2016-11-15 09:07:48 -0800

    frontend: fix MacOS install command
    Reported at https://github.com/coreos/etcd/issues/6853.

--- frontend/app/doc/tip/common/etcd.component.ts
+++ frontend/app/doc/tip/common/etcd.component.ts
@@ -325,7 +325,7 @@ GOOGLE_URL=https://storage.googleapis.com/etcd
 ` + 'rm -rf /tmp/test-etcd-${ETCD_VER}' + `
 
 ` + 'curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-darwin-amd64.zip -o /tmp/etcd-${ETCD_VER}-darwin-amd64.zip' + `
-` + 'unzip /tmp/etcd-${ETCD_VER}-darwin-amd64.zip -d /tmp && mv /tmp/etcd-${ETCD_VER}-darwin-amd64 /tmp/test-etcd' + `
+` + 'unzip /tmp/etcd-${ETCD_VER}-darwin-amd64.zip -d /tmp && mv /tmp/etcd-${ETCD_VER}-darwin-amd64/* /tmp/test-etcd' + `
 
 `;
         if (this.getExecDir() === '/') {
