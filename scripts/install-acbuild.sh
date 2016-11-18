e8275801c6d8165d7588e5792d2f708f80c4d3bd
Gyu-Ho Lee <gyuhox@gmail.com>
2016-11-18 13:16:10 -0800

    scripts: add acbuild script (WIP)

--- scripts/install-acbuild.sh
+++ scripts/install-acbuild.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+if ! [[ "$0" =~ "scripts/install-acbuild.sh" ]]; then
+    echo "must be run from repository root"
+    exit 255
+fi
+
 ACBUILD_VER=v0.4.0
 DOWNLOAD_URL=https://github.com/containers/build/releases/download
 
