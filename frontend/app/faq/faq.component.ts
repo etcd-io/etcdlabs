d6bf3e91511af1c09608e831c7aff735d511bae8
Gyu-Ho Lee <gyuhox@gmail.com>
2016-12-06 19:57:18 -0800

    frontend: clean up, remove FAQ

--- frontend/app/faq/faq.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-faq',
-    templateUrl: 'faq.component.html',
-    styleUrls: ['common.component.css'],
-})
-export class FAQComponent {
-}
