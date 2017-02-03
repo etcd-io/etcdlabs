a558e684f58d7af02aea44121c54a755ee1b9441
Gyu-Ho Lee <gyuhox@gmail.com>
2017-02-02 23:59:57 -0800

    Update tests.sh

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -12,22 +12,21 @@ fi
 IGNORE_PKGS="(vendor)"
 TESTS=`find . -name \*_test.go | while read a; do dirname $a; done | sort | uniq | egrep -v "$IGNORE_PKGS"`
 
-# TODO: run with multi-CPU (not right now because of port conflict)
-echo "Running tests..."
-go test -v $TESTS;
-
 echo "Checking gofmt..."
-fmtRes=$(gofmt -l -s $TESTS 2>&1 >/dev/null)
+fmtRes=$(gofmt -l -s -d $TESTS)
 if [ -n "${fmtRes}" ]; then
 	echo -e "gofmt checking failed:\n${fmtRes}"
 	exit 255
 fi
-
 echo "Checking govet..."
 vetRes=$(go vet $TESTS 2>&1 >/dev/null)
 if [ -n "${vetRes}" ]; then
 	echo -e "govet checking failed:\n${vetRes}"
 	exit 255
 fi
+
+# TODO: run with multi-CPU (not right now because of port conflict)
+echo "Running tests..."
+go test -v $TESTS;
 
 echo "Success";
