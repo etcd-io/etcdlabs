5d8f8675480fa393f797eebdf7c7f31d36bbe561
Gyu-Ho Lee <gyuhox@gmail.com>
2017-02-15 14:03:36 -0800

    frontend: deleting 'rkt' commands

--- frontend/app/install/common/rkt.component.ts
+++ frontend/app/install/common/rkt.component.ts
@@ -36,33 +36,4 @@ export class Rkt {
     stripVersion() {
         return this.version.substring(1)
     }
-
-    getInstallCommandLinux() {
-        let divide = getDivider(this.execDir);
-
-        let txt = `RKT_VERSION=${this.version}
-
-GITHUB_URL=https://github.com/coreos/rkt/releases/download
-
-` + 'DOWNLOAD_URL=${GITHUB_URL}' + `
-
-`;
-
-        txt += 'rm -f /tmp/rkt-${RKT_VERSION}.tar.gz' + `
-` + 'rm -rf /tmp/test-rkt-${RKT_VERSION} && mkdir -p /tmp/test-rkt-${RKT_VERSION}' + `
-
-` + 'curl -L ${DOWNLOAD_URL}/${RKT_VERSION}/rkt-${RKT_VERSION}.tar.gz -o /tmp/rkt-${RKT_VERSION}.tar.gz' + `
-` + 'tar xzvf /tmp/rkt-${RKT_VERSION}.tar.gz -C /tmp/test-rkt-${RKT_VERSION} --strip-components=1' + `
-
-`;
-        if (this.execDir === '/') {
-            txt += '# sudo cp /tmp/test-rkt-${RKT_VERSION}/rkt /usr/local/bin' + `
-`;
-        }
-        txt += 'sudo cp /tmp/test-rkt-${RKT_VERSION}/rkt ' + this.execDir + `
-
-` + this.execDir + divide + `rkt version`;
-
-        return txt;
-    }
 }
