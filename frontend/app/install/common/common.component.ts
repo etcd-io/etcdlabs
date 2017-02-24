ca30505c626c942cee63e6458e70261e1f73bae5
Gyu-Ho Lee <gyuhox@gmail.com>
2017-02-24 13:18:13 -0800

    Update common.component.ts

--- frontend/app/install/common/common.component.ts
+++ frontend/app/install/common/common.component.ts
@@ -1,7 +1,7 @@
 export class ParentComponent {
     latestReleaseVersion: string;
     constructor() {
-        this.latestReleaseVersion = 'v3.1.1';
+        this.latestReleaseVersion = 'v3.1.2';
     }
     getLatestReleaseVersion() {
         return this.latestReleaseVersion;
