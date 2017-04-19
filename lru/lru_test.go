cf2157929c71f4f3eba3d2479041fa22cc171fc3
Gyu-Ho Lee <gyuhox@gmail.com>
2017-04-19 10:28:02 -0700

    lru: test eviction
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- lru/lru_test.go
+++ lru/lru_test.go
@@ -10,7 +10,19 @@ func TestNewInMemory(t *testing.T) {
 	if err := c.Put("foo", "bar"); err != nil {
 		t.Fatal(err)
 	}
-	if v, err := c.Get("foo"); err != nil || fmt.Sprint(v) != "bar" {
+	if err := c.Put("foo1", "bar"); err != nil {
 		t.Fatal(err)
+	}
+	if err := c.Put("foo2", "bar"); err != nil {
+		t.Fatal(err)
+	}
+	if err := c.Put("foo3", "bar"); err != nil {
+		t.Fatal(err)
+	}
+	if v, err := c.Get("foo3"); err != nil || fmt.Sprint(v) != "bar" {
+		t.Fatal(err)
+	}
+	if _, err := c.Get("foo"); err != ErrKeyNotFound {
+		t.Fatalf("expected eviction with %v, got %v", ErrKeyNotFound, err)
 	}
 }
