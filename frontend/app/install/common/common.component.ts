2ce56e84d4f5bc25211836c34c24dfcf1594f417
Gyu-Ho Lee <gyuhox@gmail.com>
2017-04-28 08:43:36 -0700

    frontend: bump up etcd latest version to 'v3.2.0-rc.0'

--- frontend/app/install/common/common.component.ts
+++ frontend/app/install/common/common.component.ts
@@ -1,7 +1,7 @@
 export class ParentComponent {
     latestReleaseVersion: string;
     constructor() {
-        this.latestReleaseVersion = 'v3.1.6';
+        this.latestReleaseVersion = 'v3.2.0-rc.0';
     }
     getLatestReleaseVersion() {
         return this.latestReleaseVersion;
