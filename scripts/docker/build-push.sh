e9677baea54f9267d1d4c6987f658c6e7260d1aa
Gyu-Ho Lee <gyuhox@gmail.com>
2017-08-23 14:05:38 -0700

    vendor: sync with etcd master, upgrade frontend deps
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- scripts/docker/build-push.sh
+++ scripts/docker/build-push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if ! [[ "$0" =~ "scripts/docker/build-push.sh" ]]; then
+if ! [[ "$0" =~ "./scripts/docker/build-push.sh" ]]; then
     echo "must be run from repository root"
     exit 255
 fi
