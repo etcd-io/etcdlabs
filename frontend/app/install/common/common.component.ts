9b667742667dfa77b5e89c14982077b50b164350
Gyu-Ho Lee <gyuhox@gmail.com>
2017-09-08 15:28:25 -0700

    frontend/app: upgrade etcd latest version
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- frontend/app/install/common/common.component.ts
+++ frontend/app/install/common/common.component.ts
@@ -1,7 +1,7 @@
 export class ParentComponent {
     latestReleaseVersion: string;
     constructor() {
-        this.latestReleaseVersion = 'v3.2.7';
+        this.latestReleaseVersion = 'v3.2.8';
     }
     getLatestReleaseVersion() {
         return this.latestReleaseVersion;
