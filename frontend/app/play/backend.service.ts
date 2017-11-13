dedf018242c2411bc1527754d9b15ceff232b658
Gyu-Ho Lee <gyuhox@gmail.com>
2017-11-13 09:34:36 -0800

    frontend/app: fix backend.service.ts
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- frontend/app/play/backend.service.ts
+++ frontend/app/play/backend.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs';
+import 'rxjs/Rx';
 
 // Connect contains initial server state.
 export class Connect {
