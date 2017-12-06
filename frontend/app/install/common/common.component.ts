28cfaed0e08e4712d818ab95f0fb3113cf80a338
Gyuho Lee <gyuhox@gmail.com>
2017-12-06 12:13:57 -0800

    frontend: bump up etcd version to v3.2.11
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- frontend/app/install/common/common.component.ts
+++ frontend/app/install/common/common.component.ts
@@ -1,7 +1,7 @@
 export class ParentComponent {
     latestReleaseVersion: string;
     constructor() {
-        this.latestReleaseVersion = 'v3.2.10';
+        this.latestReleaseVersion = 'v3.2.11';
     }
     getLatestReleaseVersion() {
         return this.latestReleaseVersion;
