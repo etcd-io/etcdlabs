367bcaa47ea0f6ad0794dd52b8ef2dd031d67dce
Gyuho Lee <gyuhox@gmail.com>
2017-12-21 12:05:01 -0800

    backend/web: count users in second-scale
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- backend/web/handler.go
+++ backend/web/handler.go
@@ -86,7 +86,7 @@ func withCache(h ContextHandler) ContextHandler {
 	return ContextHandlerFunc(func(ctx context.Context, w http.ResponseWriter, req *http.Request) error {
 		userID := generateUserID(req)
 		globalServerVisitsMu.Lock()
-		globalServerVisits.Insert([]byte(userID + time.Now().String()[:10]))
+		globalServerVisits.Insert([]byte(fmt.Sprintf("%s%016X", userID, time.Now().Unix())))
 		globalServerVisitsMu.Unlock()
 		ctx = context.WithValue(ctx, userKey, &userID)
 
