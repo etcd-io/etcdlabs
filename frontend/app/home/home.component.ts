32119f312c0f63fb8c83f50ec687bdd432a14277
Gyuho Lee <gyuhox@gmail.com>
2017-12-21 12:00:44 -0800

    frontend/app: make /home SEO friendly
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- frontend/app/home/home.component.ts
+++ frontend/app/home/home.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(meta: Meta, title: Title) {
+    title.setTitle('etcd Labs');
+
+    meta.addTags([
+      { name: 'author', content: 'etcd'},
+      { name: 'keywords', content: 'etcd, etcd demo, etcd setting, etcd cluster, etcd install'},
+      { name: 'description', content: 'This is etcd demo, install guides!' }
+    ]);
+  }
+
+  ngOnInit(): void {
+  }
 }
