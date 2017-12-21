29db308064de4b6bb29067c4efa4c3964b35086e
Gyuho Lee <gyuhox@gmail.com>
2017-12-21 10:54:00 -0800

    frontend/app: upgrade etcd version to v3.3.0-rc.0
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- frontend/app/install/common/common.component.ts
+++ frontend/app/install/common/common.component.ts
@@ -1,7 +1,7 @@
 export class ParentComponent {
     latestReleaseVersion: string;
     constructor() {
-        this.latestReleaseVersion = 'v3.2.11';
+        this.latestReleaseVersion = 'v3.3.0-rc.0';
     }
     getLatestReleaseVersion() {
         return this.latestReleaseVersion;
