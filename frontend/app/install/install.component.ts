4422fc56d8f9d53f5a819510ff2b04e8603a9d68
Gyuho Lee <gyuhox@gmail.com>
2018-01-29 14:53:24 -0800

    frontend: use Go 1.9.3
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- frontend/app/install/install.component.ts
+++ frontend/app/install/install.component.ts
@@ -52,7 +52,7 @@ export class InstallComponent extends ParentComponent {
         ///////////////////////////////////////////////////
 
         ///////////////////////////////////////////////////
-        this.go = new Go('1.10');
+        this.go = new Go('1.9.3');
         this.inputGitUser = 'coreos';
         this.inputGitBranch = 'master';
         ///////////////////////////////////////////////////
