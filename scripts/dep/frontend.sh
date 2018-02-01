28842d41c8a91e4657306732439a3bf1fcddce3d
Gyuho Lee <gyuhox@gmail.com>
2018-02-01 12:31:20 -0800

    *: upgrade frontend dependencies
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- scripts/dep/frontend.sh
+++ scripts/dep/frontend.sh
@@ -11,7 +11,7 @@ SKIP_REBUILD=1 ./scripts/dep/frontend.sh
 COMMENT
 
 source ${NVM_DIR}/nvm.sh
-nvm install v9.3.0
+nvm install v9.5.0
 
 echo "Updating frontend dependencies with 'yarn' and 'npm'..."
 rm -f ./package-lock.json
@@ -25,8 +25,8 @@ else
 fi
 npm install
 
-nvm install v9.3.0
-nvm alias default 9.3.0
+nvm install v9.5.0
+nvm alias default 9.5.0
 nvm alias default node
 which node
 node -v
