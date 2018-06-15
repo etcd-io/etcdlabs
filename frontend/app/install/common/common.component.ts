144efef09280ac65abd6351b40f96c0c4539d4f0
Gyuho Lee <gyuhox@gmail.com>
2018-06-15 10:55:55 -0700

    frontend/app/install: upgrade Go and etcd versions
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- frontend/app/install/common/common.component.ts
+++ frontend/app/install/common/common.component.ts
@@ -1,7 +1,7 @@
 export class ParentComponent {
     latestReleaseVersion: string;
     constructor() {
-        this.latestReleaseVersion = 'v3.3.7';
+        this.latestReleaseVersion = 'v3.3.8';
     }
     getLatestReleaseVersion() {
         return this.latestReleaseVersion;
--- frontend/app/install/install.component.ts
+++ frontend/app/install/install.component.ts
@@ -61,7 +61,7 @@ export class InstallComponent extends ParentComponent {
         ///////////////////////////////////////////////////
 
         ///////////////////////////////////////////////////
-        this.go = new Go('1.10.2');
+        this.go = new Go('1.10.3');
         this.inputGitUser = 'coreos';
         this.inputGitBranch = 'master';
         ///////////////////////////////////////////////////
