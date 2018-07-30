a235b239006cfafcee4d0da90cf77e32db1c9700
Gyuho Lee <gyuhox@gmail.com>
2018-07-30 10:52:02 -0700

    scripts/docker: update
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- scripts/docker/build-push.sh
+++ scripts/docker/build-push.sh
@@ -7,9 +7,6 @@ if ! [[ "$0" =~ "./scripts/docker/build-push.sh" ]]; then
 fi
 
 docker build --tag gcr.io/etcd-development/etcdlabs:latest --file ./Dockerfile .
-
-gcloud docker -- login -u _json_key -p "$(cat /etc/gcp-key-etcd-development.json)" https://gcr.io
-
+gcloud docker -- login -u _json_key -p "$(cat ${HOME}/a/keys/gcp-key-etcd-development.json)" https://gcr.io
 gcloud docker -- push gcr.io/etcd-development/etcdlabs:latest
-
 gsutil -m acl ch -u allUsers:R -r gs://artifacts.etcd-development.appspot.com
